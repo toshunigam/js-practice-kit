4277badd7192c01d79ab715e292b3829d8e35c62
toshunigam <toshuchandra@gmail.com>
2021-09-19 22:36:00 +0530

    working into lodash examples

--- src/array/array-example.js
+++ src/array/array-example.js
@@ -3,6 +3,20 @@ const _ = require('lodash')
 var arr = [1,23,1,34,44,32,10,56,9,03,68,23,98]
 
 const chunking = _.chunk(arr,4)
-
+// [ [ 1, 23, 1 ], [ 34, 44, 32 ], [ 10, 56, 9 ], [ 3, 68, 23 ], [ 98 ] ]
 // console.log(chunking)
 
+/* 
+* compact example
+*/
+
+const compact = _.compact([false,true,"",null,3,undefined,6])
+// [true,3,6]
+// console.log(compact)
+
+/*
+* findIndex example
+*/
+
+const index = _.findIndex(arr,function(item){return item==32})
+// console.log(index)
