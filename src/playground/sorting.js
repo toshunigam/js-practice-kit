27837fceb37e07d2fd491c6871a86cd675dc2cb6
toshunigam <toshuchandra@gmail.com>
2021-09-29 00:03:31 +0530

    worked into sorting an array

--- src/playground/sorting.js
+++ src/playground/sorting.js
@@ -1,14 +1,11 @@
-var arr = [21,1,23,4,56,53,323,4,0]
-var sortAr=[]
-var temp = null
-for(let i=0;i<=arr.length;i++){
-    
-    for(let j=0;j<=arr.length-1;j++){
-        temp = arr[i]
-        if(arr[j]>temp){
-            temp=arr[j]
+var arr = [21,1,23,4,56,53,-323,4,0]
+for(let i=0;i<=arr.length-1;i++){
+    for(let j=0;j<=arr.length;j++){
+        if(arr[j]<arr[j+1]){
+            let temp = arr[j]
+            arr[j]=arr[j+1]
+            arr[j+1]=temp
         }
     }
-    sortAr.push(temp)
 }
-console.log(sortAr)
+console.log(arr)
