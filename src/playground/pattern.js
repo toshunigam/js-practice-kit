7e40d230e2041a3b95d738f424a7adedf5390145
toshunigam <toshuchandra@gmail.com>
2022-01-02 14:04:24 +0530

    working into pattern

--- src/playground/pattern.js
+++ src/playground/pattern.js
@@ -51,12 +51,60 @@ function patternC(n){
 //     * * * 
 //   * * * * 
 // * * * * * 
-    for(let i=n; i>n; i--){
-        let identity = '';
-        for(let j=i; j<n; j--){
+    for(let i=0; i<n;i++){
+        let identity = '1'
+        for(let j=n;j>i;j--){
+            identity += '*'
+        }
+        console.log(identity)
+    
+    }
+}
+// patternC(4)
+
+function patternD(n){
+    var c = n
+    for(let i=0; i< 2*n-1;i++){
+        let identity = ''
+        if(c>i){
+            for(let j=0;j<=i;j++){
+                identity += '*'
+            }
+        }else{
+           for(let k=(i-c+1);k<c;k++){
+            identity += '*'
+           }
+        }
+        console.log(identity)
+    }
+}
+// patternD(5)
+
+function patternD_1(n){
+    for(let i=0; i< 2*n; i++){
+        let totalCol = i>n ? 2*n-i:i
+        let identity = ''
+        for(let j=0; j< totalCol; j++){
             identity += '*'
         }
         console.log(identity)
     }
 }
-patternC(5)
+
+// patternD_1(5)
+
+function patternE(n){
+    for(let i=0; i< 2*n; i++){
+        let totalCol = i>n ? 2*n-i:i
+        let identity = ''
+        let noOfSpaces = n-totalCol
+        for(let s=0; s<noOfSpaces;s++){
+            identity = ' '
+        }
+        for(let j=0; j< totalCol; j++){
+            identity += '*'
+        }
+        console.log(identity)
+    }
+}
+patternE(5)
