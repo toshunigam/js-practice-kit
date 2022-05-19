79afa133eb0a305928053799a705ca9dac6cc6e8
toshunigam <toshuchandra@gmail.com>
2022-05-19 22:30:11 +0530

    working into date

--- src/playground/date-example.js
+++ src/playground/date-example.js
@@ -6,9 +6,22 @@ let curdate = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate()
 // console.log(d.getMonth(), curdate,dob)
 
 let date1 = new Date(dob)
-let date2 = new Date(curdate)
-let dateDiff = date2 - date1
-dateDiff = Math.ceil((dateDiff)/(1000*24*60*60))
-console.log(Math.ceil(dateDiff/365), 'year')
+let date2 = new Date()
+let dateDiff = Math.floor(date2.getTime() - date1.getTime())
+let dateDiff2 = Math.ceil((dateDiff)/(1000*24*60*60))
+console.log(Math.ceil(dateDiff2/365), 'year')
+let day = 1000*60*60*24
+let days = Math.floor(dateDiff/day)
+let month = Math.floor(days/31)
+let year = Math.floor(month/12)
 
+console.log('AGE: Year ',year,' Month ',month,' days ',days)
 // convert timestamp into date
+
+const dd = new Date(dateDiff)
+console.log(dd.getDate()+'/'+(dd.getMonth()+1)+'/'+dd.getFullYear())
+
+
+today = new Date()
+past = new Date(1987,05,23) // remember this is equivalent to 06 01 2010
+//dates in js are counted from 0, so 05 is june
