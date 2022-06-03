9f68d58235e7eefb37b36f3ea084f37646c5ca78
toshunigam <toshuchandra@gmail.com>
2022-06-03 23:16:54 +0530

    working into object

--- src/playground/object-example.js
+++ src/playground/object-example.js
@@ -18,3 +18,8 @@ let permissions2 = { canEdit: true };
 // copies all properties from permissions1 and permissions2 into user
 Object.assign(user, permissions1, permissions2);
 console.log(user)
+
+const jsonObj = {'a':1,'b':2,'c':3,'d':4,'e':5}
+for(const [key,value] of Object.entries(jsonObj)){
+    console.log(`${key} : ${value}`)
+}
