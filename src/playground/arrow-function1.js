2afcb30b387e7b4ed88168d36cbabfa4ea90af0d
toshunigam <toshuchandra@gmail.com>
2023-02-15 21:32:26 +0530

    practice arrow function

---
+++ src/playground/arrow-function1.js
@@ -0,0 +1,9 @@
+let exm = {
+    "name":"toshu",
+    "age": sayAge = () =>{
+        return "your age is 35"
+    },
+    "email":"toshu44heman@gmail.com"
+}
+console.log(exm.name)
+console.log(exm.age())
