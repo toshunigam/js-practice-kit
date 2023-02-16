69717d0c7dba3f693547e92537bae64f2483041c
toshunigam <toshuchandra@gmail.com>
2023-02-16 20:36:34 +0530

    javascript

--- src/playground/arrow-function1.js
+++ src/playground/arrow-function1.js
@@ -1,9 +1,9 @@
 let exm = {
     "name":"toshu",
-    "age": sayAge = () =>{
-        return "your age is 35"
+    "age": (age) => {
+        return `your age is ${age}`
     },
     "email":"toshu44heman@gmail.com"
 }
 console.log(exm.name)
-console.log(exm.age())
+console.log(exm.age(12))
