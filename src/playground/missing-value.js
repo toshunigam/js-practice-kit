effa53c2d4c40467c75e8e40f3a271eb9bf1f576
toshunigam <toshuchandra@gmail.com>
2023-07-15 22:40:26 +0530

    working into passing argument into runtime

--- src/playground/missing-value.js
+++ src/playground/missing-value.js
@@ -1,6 +1,7 @@
 let arr = [4,3,1,3,5,7]
+console.log("Given Array input: ",arr);
 var output = []
-
+//sorting
 for(let i=0; i<arr.length;i++){
     for(let j=0; j<=arr.length;j++){
         if(arr[j]>arr[j+1]){
@@ -10,14 +11,17 @@ for(let i=0; i<arr.length;i++){
         }
     }
 }
-
+//sorted array
+console.log('Sorted Array: ',arr);
+//finding missing values
 let missing = []
 for(let i=0;i<arr.length;i++){
     if(arr[i]!==i+1){
         missing.push(i+1)
     }
 }
-
+console.log('Missing value: ', missing);
+//finding multiple values
 let multipleVal = []
 for(let t=0;t<arr.length;t++){
     let count=0
@@ -33,6 +37,4 @@ for(let t=0;t<arr.length;t++){
         multipleVal.push(arr[t])
     }
 }
-console.log(arr)
-console.log(missing)
-console.log(multipleVal)
+console.log("Multiple values: ",multipleVal)
