5e188dad2eb4d195fc5b5e138991761f0f4810a3
toshunigam <toshuchandra@gmail.com>
2023-07-16 18:12:48 +0530

    append in new line

--- src/files/exa2.js
+++ src/files/exa2.js
@@ -1,6 +1,7 @@
+// example of writing/appending data into file
 const fs = require("fs")
 fs.open('./text.txt', 'a', 666, function(e, id){
-    fs.write(id, process.argv[2], null, 'utf8', function(){
+    fs.write(id, process.argv[2]+"\n", null, 'utf8', function(){
         fs.close(id, function(){
             console.log('File is closed')
         })
